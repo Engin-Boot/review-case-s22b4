0c876d17714c9ba58ab89b8afacf341a5038f998
anjalimnnit <anjalishah.mnnit@gmail.com>
2020-09-14 18:18:50 +0530

    resolved tests

--- Receiver.Test/ReceiverUnitTest.cs
+++ Receiver.Test/ReceiverUnitTest.cs
@@ -29,13 +29,13 @@ namespace Receiver.Test
         {
             var wordCount = new WordFrequencyGenerator();
             var record1 = new CommentRecord(new StringBuilder("1/1/2020 12:30"),new StringBuilder("Code should be decoupled"));
-            var record2 = new CommentRecord(new StringBuilder("20/ 2/2020 13:10"), new StringBuilder("No additional Comments"));
-            var record3 = new CommentRecord(new StringBuilder("10/ 3/2020 19:09"), new StringBuilder("No additional Comments"));
+            var record2 = new CommentRecord(new StringBuilder("20/2/2020 13:10"), new StringBuilder("No additional Comments"));
+            var record3 = new CommentRecord(new StringBuilder("10/3/2020 19:09"), new StringBuilder("No additional Comments"));
             var commentRecord = new List<CommentRecord> { record1, record2, record3 };
             var frequencyList1 = new Dictionary<string, int> { ["code"] = 1, ["should"] = 1, ["be"] = 1, ["decoupled"] = 1, ["no"] = 2, ["additional"] = 2, ["comments"] = 2 };
             var frequencyList2 = new Dictionary<string, int> { ["code"] = 1, ["decoupled"] = 1, ["additional"] = 2, ["comments"] = 2 };
             Assert.True(wordCount.GenerateFrequencyList(commentRecord,"path2.csv").SequenceEqual(frequencyList2));
-            Assert.False(wordCount.GenerateFrequencyList(commentRecord,"path2.csv").SequenceEqual(frequencyList1));
+            Assert.False(wordCount.GenerateFrequencyList(commentRecord,"path3.csv").SequenceEqual(frequencyList1));
 
         }
         
